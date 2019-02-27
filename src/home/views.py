0d59b434ecce6a7c5d1470fcc0d55c4b56de50aa
Kandai Watanabe <kandai.wata@gmail.com>
2019-02-27 13:38:07 +0900

    managed to load title and image from gunosy website

--- src/home/views.py
+++ src/home/views.py
@@ -1,2 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HomeForm
+from scripts.utils import html_to_text_img
+# from scripts.classifier import Classifier
+
+# cf = Classifier('NaiveBayes')
+
+def home(request):
+    url = request.GET.get('url')
+    article_text, img_url= html_to_text_img(url)
+    
+    # category = cf.classify(article_text)
+
+    if article_text is None:
+        category = "Please enter URL."
+    else: 
+        category = article_text
+    if img_url is not None:
+        img_url = 'https:' + img_url
+
+    return render(request, 'form.html', {
+                    'category': category, 
+                    'img_url': img_url}
+                )
+
---
+++ src/scripts/utils.py
@@ -0,0 +1,29 @@
+# 命名規則： PEP8
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
+import logging
+
+def html_to_text_img(url):
+    if url is '' or url is None:
+        return None
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    except URLError as e:
+        print(e)
+        return None
+
+    try:
+        soup = BeautifulSoup(html.read(), "html.parser")
+    except AttributeError as e:
+        print(e)
+        return None
+
+    text = soup.find("h1").get_text()
+    imgs = soup.select("div.article__image img")
+    first_img_url = imgs[0]['data-src']
+
+    return text, first_img_url
