d54953e2c5e23c7ed77896ddb0ecadf4a03f7dad
Kandai Watanabe <kandai.wata@gmail.com>
2019-03-04 16:00:17 +0900

    added new model to db

--- src/doc_classify/settings.py
+++ src/doc_classify/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'doc_classify.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'docclassify.db'),
     }
 }
 
--- src/home/models.py
+++ src/home/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Article(models.Model):
+    category = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
