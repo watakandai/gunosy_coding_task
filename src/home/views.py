3cdff3a3e4411ec1a0a111bfca9f00ad97e160b4
Kandai Watanabe <kandai.wata@gmail.com>
2019-03-08 12:17:15 +0900

    added classifier to home view for homepage

--- src/home/views.py
+++ src/home/views.py
@@ -1,21 +1,44 @@
+import dill
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import HomeForm
-from scripts.utils import html_to_text_img
-# from scripts.classifier import Classifier
+from scripts.html_to_text_img import html_to_text_img
+from scripts.load_data import MorphologicalAnalyzer as MA
+import sys
+from scripts import classifiers
+sys.modules['classifiers'] = classifiers
 
-# cf = Classifier('NaiveBayes')
+categories = {
+    'https://gunosy.com/categories/1': 'ENTERTAINMENT',
+    'https://gunosy.com/categories/2': 'SPORT',
+    'https://gunosy.com/categories/3': 'GAGS',
+    'https://gunosy.com/categories/4': 'DOMESTIC',
+    'https://gunosy.com/categories/5': 'INTERNATIONAL',
+    'https://gunosy.com/categories/6': 'COLUMN',
+    'https://gunosy.com/categories/7': 'IT',
+    'https://gunosy.com/categories/8': 'GOURMET',
+}
+category_lists = list(categories.values())
+nb = classifiers.NaiveBayes(T=category_lists)
+nb.load_params()
+ma = MA(word_class=["名詞", "動詞"])
 
 
 def home(request):
+    global nb
     url = request.GET.get('url')
-    article_text, img_url = html_to_text_img(url)
+    print(url)
+    if url is not None:
+        article_text, img_url = html_to_text_img(url)
+        words = ma.split(article_text)
+    else:
+        article_text = None
+        img_url = None
     # category = cf.classify(article_text)
 
     if article_text is None:
         category = "Please enter URL."
     else:
-        category = article_text
+        category = nb.predict(words)
     if img_url is not None:
         img_url = 'https:' + img_url
 
